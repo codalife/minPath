d88f961fc33a87e533a6e4efe752d318dfcc7599
codalife <anuar.zhukenov@gmail.com>
2018-03-02 20:57:46 -0800

    fix

--- minPathFinder.js
+++ minPathFinder.js
@@ -1,16 +1,41 @@
-const dictionary = require('./WebstersEnglishDictionary/dictionary.json');
+/*
+const fs = require('fs');
+fs.readFile(
+  './google-10000-english/google-10000-english-no-swears.txt',
+  'utf8',
+  (err, res) => {
+    const arrfWords = res.split('\n').filter(word => word.length > 2);
+    fs.writeFileSync('./setOfWords.json', JSON.stringify(arrfWords));
+  },
+);
+*/
 
-const setOfWords = Object.keys(dictionary).reduce((dic, word) => {
-  if (word.length > 2) {
-    dic.add(word);
-  }
-  return dic;
-}, new Set());
+const words = require('./setOfWords.json');
+
+const setOfWords = new Set(words);
+
+// Object.keys(dictionary).reduce((dic, word) => {
+//   if (word.length > 2) {
+//     dic.add(word);
+//   }
+//   return dic;
+// }, setOfWords);
+
+[
+  'heath',
+  'heats',
+  'hents',
+  'hends',
+  'hands',
+  'team',
+  'mate',
+  'glasses',
+  'OPHTHALMOLOGY',
+].forEach(word => setOfWords.add(word));
 
 class MinPathFinder {
   constructor(dictionary) {
     this.dictionary = dictionary;
-    // console.log(this.dictionary);
   }
 
   calculateMinPath(costs, word1, word2) {
@@ -20,27 +45,20 @@ class MinPathFinder {
     let result = Number.MAX_SAFE_INTEGER;
 
     const recursiveFind = (from, to, soFar) => {
-      //   console.log('*********************');
-      console.log(`word1: ${from}, word2: ${to}`);
-      //   console.log(seen);
       if (from === to) {
-        console.log(`found similar words`);
         result = Math.min(result, soFar);
         return;
       }
       if (soFar >= result || seen.has(from)) {
-        // console.log(`hit destination because ${this.dictionary.has(from)}`);
         return;
       }
 
       seen.add(from);
 
       if (from.length !== to.length) {
-        // console.log(`see that lengths are not equal`);
         if (from.length > to.length) {
           for (let i = 0; i < from.length; i += 1) {
             let candidate = from.substring(0, i) + from.substring(i + 1);
-            console.log(candidate);
             if (this.dictionary.has(candidate)) {
               recursiveFind(candidate, to, soFar + deleteLetter);
             }
@@ -48,24 +66,14 @@ class MinPathFinder {
         } else {
           for (let j = 0; j < to.length; j += 1) {
             let candidate = to.substring(0, j) + to.substring(j + 1);
-            console.log(candidate);
             if (this.dictionary.has(candidate)) {
               recursiveFind(from, candidate, soFar + deleteLetter);
             }
           }
-        } // recursiveFind(from.slice(-1), to, soFar + deleteLetter);
-
-        for (let i = 0; i < from.length; i += 1) {
-          let candidate = from.substring(0, i) + from.substring(i + 1);
-          console.log(candidate);
-          if (this.dictionary.has(candidate)) {
-            recursiveFind(candidate, to, soFar + deleteLetter);
-          }
         }
       } else {
-        // console.log(`Equal lengths words`);
         if (this.areAnagrams(from, to)) {
-          result = Max.min(result, soFar + takeAnagram);
+          result = Math.min(result, soFar + takeAnagram);
 
           recursiveFind(from, to);
         }
@@ -85,32 +93,38 @@ class MinPathFinder {
           }
         }
       }
+      seen.delete(from);
     };
 
-    recursiveFind(word1, word2, 0);
+    recursiveFind(word1.toLowerCase(), word2.toLowerCase(), 0);
 
-    return result;
+    return result === Number.MAX_SAFE_INTEGER ? -1 : result;
   }
   areAnagrams(word1, word2) {
-    return (
-      word1
-        .split()
-        .sort()
-        .join() ===
-      word2
-        .split()
-        .sort()
-        .join()
-    );
+    const candidate1 = word1
+      .split('')
+      .sort()
+      .join('');
+    const candidate2 = word2
+      .split('')
+      .sort()
+      .join('');
+
+    return candidate1 === candidate2;
   }
 }
 
-const testWord1 = 'suit';
-const testWord2 = 'suite';
+const testWord1 = 'ophtalmology';
+const testWord2 = 'glasses';
 const testCosts = [1, 3, 1, 5];
 
 const solver = new MinPathFinder(setOfWords);
 
 const result = solver.calculateMinPath(testCosts, testWord1, testWord2);
-// let result = 'hello';
+
+const result1 = solver.calculateMinPath([1, 3, 1, 5], 'health', 'hands');
+const result2 = solver.calculateMinPath([1, 9, 1, 3], 'team', 'mate');
+console.log(`result with team and mate is ${result2}`);
+console.log(`result with health and hands is ${result1}`);
+
 console.log(`result is ${result}`);
